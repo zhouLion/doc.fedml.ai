50feb2bc9f19c974297ebee234026323a2f7a290
zhouLion <zhouquan147369@gmail.com>
2023-09-10 08:45:20 +0800

    chore: set docs' routeBasePath to root path

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -76,6 +76,7 @@ const config = {
       ({
         docs: {
           ...defaultSettings,
+          routeBasePath: '/',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
