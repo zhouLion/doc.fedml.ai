9eb9330252468ee385c22c41b78970a5d4bebeea
Chaoyang He <chaoyanghe.com@gmail.com>
2023-10-28 21:57:30 -0700

    update link for "Edit this Page"

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -81,15 +81,8 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/FedML-AI/docs.fedml.ai/tree/main/',
         },
-        // blog: {
-        //   showReadingTime: true,
-        //   // Please change this to your repo.
-        //   // Remove this to remove the "edit this page" links.
-        //   editUrl:
-        //     'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        // },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
