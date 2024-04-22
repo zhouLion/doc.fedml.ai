35361ae0a881638ab190cb885b8337ca9be7751f
Alay Shah <alay11shah@gmail.com>
2024-04-22 11:34:48 -0700

    Add Render Token <> Node Mapping Script

---
+++ static/python/render.py
@@ -0,0 +1,94 @@
+import os
+import sys
+import yaml
+import requests
+
+TOKEN_MISSING_ERROR_MESSAGE = ("\033[1;31m\u2717 Error: Render Auth Token is missing. Kindly execute the last command again, "
+                               "and enter Render Auth Token when prompted\033[0m")
+
+FEDML_CONFIG_MISSING_ERROR_MESSAGE = ("\033[1;31m\u2717 Error: Your node failed to bind to the FEDML platform. "
+                                      "Please try the binding process from start again.\033[0m")
+
+NODE_BIND_SUCCESS_MESSAGE = ("\033[1;32m\u2713 Congratulations! Your node is successfully binded to the FEDML platform!\033[0m")
+
+
+def get_user_render_token():
+    user_render_token = input("Enter your render auth token: ")
+    return user_render_token
+
+
+def read_edge_id():
+    home_dir = os.path.expanduser("~")
+    edge_id_file = os.path.join(home_dir, ".fedml/fedml-client/fedml/data/runner_infos/runner_infos.yaml")
+    try:
+        with open(edge_id_file, "r") as f:
+            runner_infos = yaml.safe_load(f)
+            edge_id = runner_infos.get("edge_id")
+    except Exception as e:
+        print(FEDML_CONFIG_MISSING_ERROR_MESSAGE)
+        sys.exit(1)
+    return edge_id
+
+
+def read_api_key():
+    home_dir = os.path.expanduser("~")
+    api_key_file = os.path.join(home_dir, ".fedml/fedml-client/fedml/data/secret/launch_secret")
+    try:
+        with open(api_key_file, "r") as f:
+            api_key = f.read().strip()
+    except Exception as e:
+        print(FEDML_CONFIG_MISSING_ERROR_MESSAGE)
+        sys.exit(1)
+    return api_key
+
+
+def send_request(render_token, edge_id, api_key):
+    url = "https://open-test.fedml.ai/cheetah/cli/web3/token-node-rel"
+    headers = {"Authorization": f"Bearer {api_key}"}
+    payload = {"nodeId": edge_id, "token": render_token}
+    response = requests.post(url, headers=headers, json=payload)
+    return response
+
+def get_public_ip():
+    try:
+        response = requests.get('https://api.ipify.org')
+        if response.status_code == 200:
+            return response.text
+        else:
+            # print("Failed to retrieve public IP. Status code:", response.status_code)
+            return None
+    except requests.RequestException as e:
+        # print("Error occurred during request:", e)
+        return None
+
+
+def validate_input(render_token, edge_id, api_key):
+    if render_token.strip() == "":
+        print(TOKEN_MISSING_ERROR)
+        sys.exit(1)
+
+    if edge_id.strip() == "" or api_key.strip() == "":
+        print(FEDML_CONFIG_MISSING_ERROR)
+        sys.exit(1)
+
+
+def main():
+    render_token = get_user_render_token()
+    edge_id = read_edge_id()
+    api_key = read_api_key()
+    public_ip = get_public_ip()
+    validate_input(render_token=render_token, edge_id=edge_id, api_key=api_key)
+    response = send_request(render_token=render_token, edge_id=edge_id, api_key=api_key)
+
+    if response.status_code == 200:
+        print(NODE_BIND_SUCCESS_MESSAGE)
+    else:
+        print("\033[1;31m\u2717 The attempt to bind the node to the FEDML platform has encountered a failure. "
+              "Please retry the binding process from the start, and if the problem persists, reach out to the FEDML Team for further assistance.",
+              response.status_code,
+              "\033[0m")
+        print("Error message:", response.text)
+
+
+if __name__ == "__main__":
+    main()
