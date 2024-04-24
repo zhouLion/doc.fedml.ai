1d1aa5dfaebfefee85d97b4d99fd9911edcfdcc8
Alay Shah <alay11shah@gmail.com>
2024-04-23 19:03:43 -0700

    Add timeout

--- static/shell/render_node_bind.sh
+++ static/shell/render_node_bind.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
+set -e
+
+cleanup() {
+    # Returning to the original directory
+    cd ..
+
+    # Removing the tmp folder and its contents
+    rm -rf "$tmp_folder"
+
+    # Cleanup function to execute before exiting
+    fedml logout && sudo pkill -9 python && sudo rm -rf ~/.fedml && redis-cli flushall
+}
+
+trap 'cleanup; echo -e "\e[31m✘ Failed to link render token to the node. Please retry the binding process again from the beginning.\e[0m"' ERR
+
 
 # Creating the tmp folder or removing it if it already exists
 tmp_folder=".render-fedml-bind"
@@ -10,7 +25,7 @@ fi
 mkdir "$tmp_folder"
 
 # Moving into the tmp folder
-cd "$tmp_folder" || exit
+cd "$tmp_folder"
 
 echo -e "\e[33m\U1F517 Linking render token to your node...\e[0m"
 sudo wget -q https://doc.fedml.ai/python/render.py && sudo chmod +x render.py
@@ -22,9 +37,4 @@ cd ..
 # Removing the tmp folder and its contents
 rm -rf "$tmp_folder"
 
-if [ $? -eq 0 ]; then
-  echo -e "\033[1;32m✔ Your token was successfully linked to this node. This concludes the node binding process.\033[0m"
-else
-  fedml logout && sudo pkill -9 python && sudo rm -rf ~/.fedml && redis-cli flushall
-  echo -e "\e[31m✘ Failed to link render token to the node. Please retry the binding process again from the beginning.\e[0m"
-fi
+echo -e "\033[1;32m✔ Your token was successfully linked to this node. This concludes the node binding process.\033[0m"
